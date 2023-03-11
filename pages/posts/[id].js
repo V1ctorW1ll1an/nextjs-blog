4ec7ad458eed5953d7c456a4597f27577c90e858
victor willian <victorwillian1012@gmail.com>
2023-03-11 17:11:20 -0300

    chore: :white_check_mark: Dynamic routers

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,42 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
